e9d54eb57b1f732f44a352db23a6ccfe50f61d80
Stéphane Englebert <stephane.englebert@gmail.com>
2021-03-05 08:50:46 +0100

    update pewpew.js

--- assets/js/pewpew.js
+++ assets/js/pewpew.js
@@ -30,7 +30,7 @@ let isTargetHit = (ctx) => {
     let hit = false;    
     let posXTarget = lSNb("posXTarget");
     let rdTarget = lSNb("radiusTarget"); 
-    let min = posXTarget - rdTarget - 2;
+    let min = posXTarget - rdTarget - 8;
     let max = posXTarget + rdTarget + 2;
     let posXShell = lSNb("posXShell");
     if((posXShell > min) && (posXShell < max)){hit = true;}
@@ -46,17 +46,24 @@ let shootShell = (ctx) => {
     let lgCn = lSNb("lgCannon");
     let htCn = lSNb("htCannon");
     let x = posXTank + lgTk/2 - lgSh/2;
-    let y = posYTank - htCn -50;
+    let y = posYTank - htCn -65;
     localStorage.posXShell = x;
     localStorage.posYShell = y;
+    playDetonation(ctx);
+    let launchedShells = lSNb("launchedShells");
+    launchedShells++;
+    localStorage.launchedShells = launchedShells;
+    showInGameMenus(ctx);
     let loop = setInterval(executeLoop,50);
     function executeLoop(){
         let posXShell = lSNb("posXShell");
         let posYShell = lSNb("posYShell");
         let offsetShell = lSNb("offsetShell");
         let hitTargets = lSNb("hitTargets");
+        let scope = 0;
         eraseShell(ctx,posXShell,posYShell);
-        if(posYShell < 80){
+        if(isTargetHit(ctx)){scope = 70;}else{scope = 5;}
+        if(posYShell <= scope){
             clearInterval(loop);
             localStorage.launched = "false";
             // check if target hit...
@@ -64,11 +71,12 @@ let shootShell = (ctx) => {
                 eraseTarget(ctx);
                 hitTargets++;
                 localStorage.hitTargets = hitTargets;
-                showHitMenu(ctx);
+                showInGameMenus(ctx);
                 if(hitTargets < 10){
                     let target = initTarget(ctx);
                 }else{
-                    // 10 targets hit - game over!                    
+                    // 10 targets hit - game over!  
+                    localStorage.gameOver = "true";
                     endOfGame(ctx);
                 }                
             }
@@ -121,15 +129,17 @@ let initScene = (ctx) => {
     localStorage.setItem("radiusTarget",20);
     localStorage.setItem("posXTarget",30);
     localStorage.setItem("posYTarget",50);
-    localStorage.setItem("launched",false);
+    localStorage.setItem("launched",false);// yes or no?
+    localStorage.setItem("launchedShells",0); // how many?
     localStorage.setItem("posXShell",0);
     localStorage.setItem("posYShell",450);
     localStorage.setItem("offsetShell",20 );
     let target = initTarget(ctx);
     localStorage.setItem("hitTargets",0);
     localStorage.setItem("elapsedTime",0);
-    showHitMenu(ctx);
-    showTimeMenu(ctx);
+    localStorage.setItem("gameOver",false);
+    localStorage.setItem("rankScores",[0,0,0,0,0]);
+    showInGameMenus(ctx);
 }
 let moveTank = (ctx,direction) => {
     let offset = lSNb("offsetTank");
@@ -142,6 +152,31 @@ let moveTank = (ctx,direction) => {
         ctx.clearRect(0,600,850,-150);
         drawTank(ctx);
     }
+}
+let playDetonation = (ctx) => {
+    let lgTank = lSNb("lgTank");
+    let htTank = lSNb("htTank");
+    let lgCan = lSNb("lgCannon");
+    let htCan = lSNb("htCannon");
+    let x = lSNb("posXTank") + (lgTank-lgCan) / 2;
+    let y = lSNb("posYTank") - (htTank/2) - htCan;
+    let detonation = {
+        draw: function(){
+            ctx.fillStyle = "yellow";     
+            ctx.beginPath();
+            ctx.moveTo(x,y);
+            ctx.lineTo(x-10,y-10);
+            ctx.lineTo(x+2,y-5);
+            ctx.lineTo(x+7,y-20);
+            ctx.lineTo(x+10,y-5);
+            ctx.lineTo(x+25,y-8);
+            ctx.lineTo(x+10,y);
+            ctx.closePath();
+            ctx.fill();
+        }
+    }
+    detonation.draw();
+    setTimeout(function(){ctx.clearRect(x-15,y,40,-25);},200);
 }
 let aleaNumber = (min,max) => {
     let alea = min + Math.floor(Math.round(Math.random() * (max-min)) * 100)/100;
@@ -158,13 +193,13 @@ let initTarget = (ctx) => {
     let target = {
         draw: function(){                       
             ctx.beginPath();
-            ctx.fillStyle = "red";
+            ctx.fillStyle = "#ffdbdb";
             ctx.arc(posXTarget,posYTarget,rdTarget,0,2 * Math.PI);
             ctx.fill();
             ctx.fillStyle = "#ffffff"; 
             ctx.arc(posXTarget,posYTarget,rdTarget-5,0,2 * Math.PI);
             ctx.stroke();
-            ctx.fillStyle = "black"; 
+            ctx.fillStyle = "red"; 
             ctx.arc(posXTarget,posYTarget,rdTarget-10,0,2 * Math.PI);
             ctx.stroke();
         }
@@ -177,35 +212,120 @@ let eraseTarget = (ctx) => {
     let rdTarget = lSNb("radiusTarget");   
     ctx.clearRect(posXTarget-rdTarget-5,0,rdTarget* 3,100);
 }
+let calculateScore = (ctx,elapsedTime,launchedShells) => {
+    let basisScore = 1000;
+    let bonus = (launchedShells < 16) ? 500 : 0;
+    console.log("bonus:"+bonus);
+    let extraBonus = (launchedShells == 10) ? 2000 : 0;
+    console.log("extrabonus:"+extraBonus);
+    let malus = (launchedShells > 20) ? -300 : 0;
+    console.log("malus:"+malus);
+    let extraMalus = (launchedShells > 25) ? -500 : 0;
+    console.log("extra malus:"+extraMalus);
+    let timeBonus = (elapsedTime < 60) ? 600 : 0;
+    console.log("time bonus:"+timeBonus);
+    let medianTimeBonus = ((elapsedTime > 59)&&(elapsedTime < 91)) ? (elapsedTime-59)*10 : 0;
+    let medianTimeBonus2 = ((elapsedTime < 60)&&(elapsedTime > 29)) ? (elapsedTime-29)*15 : 0;
+    console.log("median time bonus:"+medianTimeBonus);
+    console.log("median time bonus2:"+medianTimeBonus2);
+    let extraTimeBonus = (elapsedTime < 30) ? 1500 : 0;
+    console.log("extra time bonus:"+extraTimeBonus);
+    let timeMalus = (elapsedTime > 120) ? -200 : 0;
+    console.log("time malus"+timeMalus);
+    let extraTimeMalus = (elapsedTime > 220) ? -500 : 0;
+    console.log("extra time malus"+extraTimeMalus);
+    let scoreBonus = bonus + extraBonus;
+    let scoreMalus = malus + extraMalus;
+    let scoreTimeBonus = timeBonus + medianTimeBonus + medianTimeBonus2 + extraTimeBonus;
+    let scoreTimeMalus = timeMalus + extraTimeMalus;
+    let scoreGame = scoreBonus + scoreMalus + scoreTimeBonus + scoreTimeMalus;
+    showScore(ctx, elapsedTime, basisScore, scoreGame, scoreBonus, scoreMalus, scoreTimeBonus, scoreTimeMalus);
+    return scoreGame;
+}
+let updateRanks = (ctx) => {
+
+}
+let showScore = (ctx, elapsedTime, basisScore, scoreGame, scoreBonus, scoreMalus, scoreTimeBonus, scoreTimeMalus) => {
+    let seconds = elapsedTime%60;
+    let minutes = (elapsedTime - seconds) / 60;  
+    console.log("elap time="+elapsedTime+" / sec="+seconds+" / min="+minutes);
+    let strMin = convertStrCounter(ctx,minutes);
+    let strSec = convertStrCounter(ctx,seconds);
+    let dspString = strMin + ":" + strSec;    
+    document.getElementById("timeElapsed").innerHTML = dspString;
+    document.getElementById("basisScore").innerHTML = basisScore;
+    document.getElementById("bonus").innerHTML = scoreBonus;
+    document.getElementById("malus").innerHTML = scoreMalus;
+    document.getElementById("timeBonus").innerHTML = scoreTimeBonus;
+    document.getElementById("timeMalus").innerHTML = scoreTimeMalus;
+    document.getElementById("scoreGame").innerHTML = scoreGame;
+}
+let showRanks = (ctx, scoreGame, elapsedTime) => {
+
+}
+let showResults = (ctx, elapsedTime, launchedShells) => {
+    let scoreGame = calculateScore(ctx,elapsedTime,launchedShells);
+    updateRanks(ctx, scoreGame, elapsedTime);
+    showRanks(ctx);
+}
 let endOfGame = (ctx) => {
     ctx.clearRect(0,600,850,-150);
     localStorage.posXTank = 450;
     localStorage.posYTank = 550;
     localStorage.hitTargets = 0;
+    let elapsedTime = lSNb("elapsedTime");
+    let launchedShells = lSNb("launchedShells");
+    
+    showResults(ctx,elapsedTime,launchedShells);
+    localStorage.elapsedTime = 0;
+    localStorage.launchedShells = 0;
     let tank = drawTank(ctx);
     //let target = initTarget(ctx);
     showStartMenu();
 }
 let showHitMenu = (ctx) => {
-    ctx.clearRect(1,1,50,100);
     ctx.fillStyle = "black";
-    ctx.font = "18pt Calibri";
-    ctx.fillText("Hit", 10, 20);
+    ctx.font = "16pt Calibri";
+    ctx.fillText("Hit", 790, 170);
     ctx.fillStyle = "white";
-    ctx.fillText(localStorage.hitTargets,20,45);
+    ctx.fillText(localStorage.hitTargets,800,195);
 }
-let displayElapsedTime = (ctx) => {
+let showLaunchedMenu = (ctx) => {
+    ctx.fillStyle = "black";
+    ctx.font = "16pt Calibri";
+    ctx.fillText("Launched", 760, 95);
+    ctx.fillStyle = "white";
+    ctx.fillText(localStorage.launchedShells,800,120);
+}
+let convertStrCounter = (ctx,ctrTime) => {
+    let tempString = "00";
+    if(ctrTime>=10){tempString = ctrTime;}else{tempString = "0" + ctrTime;}
+    return tempString;
+}
+let updateElapsedTime = (ctx) => {
     let dspTime = lSNb("elapsedTime");
-    let dspString = "00 : 00";
+    dspTime++;
+    localStorage.elapsedTime = dspTime;
+    let seconds = dspTime%60;
+    let minutes = (dspTime - seconds) / 60;  
+    let strMin = convertStrCounter(ctx,minutes);
+    let strSec = convertStrCounter(ctx,seconds);
+    let dspString = strMin + " : " + strSec;
     return dspString;
 }
+let showInGameMenus = (ctx) => {
+    ctx.clearRect(760,1,90,500);
+    showHitMenu(ctx);
+    showTimeMenu(ctx);
+    showLaunchedMenu(ctx);
+}
 let showTimeMenu = (ctx) => {
-    ctx.clearRect(760,1,90,100);
     ctx.fillStyle = "black";
-    ctx.font = "18pt Calibri";
+    ctx.font = "16pt Calibri";
     ctx.fillText("Time", 780, 20);
     ctx.fillStyle = "white";
-    ctx.fillText(displayElapsedTime(ctx),770,45);
+    let insertText = updateElapsedTime(ctx);
+    ctx.fillText(insertText,770,45);
 }
 let showStartMenu = () => {
     document.getElementById("pewPewCanvas").style.display = "none";
@@ -216,6 +336,15 @@ window.onload = function (){
     showStartMenu();
     document.getElementById("start").addEventListener("click", () => {
         initScene(ctx);
+        let clock = setInterval(function(){
+            if(localStorage.gameOver == "true"){
+                //clearInterval(clock);
+                console.log("game over");
+            }else{
+                showInGameMenus(ctx);
+                console.log("game running");
+            }
+        },1000);
         document.addEventListener('keydown', (event) => {
             const keyName = event.key;
             if((keyName === "ArrowLeft")||(keyName === "ArrowRight")){moveTank(ctx,keyName);}
